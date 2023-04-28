31fa1accbb4e959d46a003f39d6491ad2944a99e
PJiyush <jhamnani.1@iitj.ac.in>
2023-04-29 01:38:10 +0530

    worked on couponBazaar componet and handled buyClick functionality

--- frontend/src/components/CouponBazaar.js
+++ frontend/src/components/CouponBazaar.js
@@ -1,16 +1,46 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { useState } from 'react'
 
 function CouponBazaar() {
-    const links=[
-        {id:1, name: "Ledger", link: "/ledger"},
-        {id:2, name: "Transaction", link: "/transaction"},
-        {id:3, name: "Student Dashboard", link: "/student"}
+    const [couponData, setCouponData] = useState([
+        { id: 1, available: 50, name: 'Food Coupon', Tokens: 100, buyed: 0 },
+        { id: 2, available: 40, name: 'Food Coupon 2', Tokens: 200, buyed: 0 },
+        { id: 3, available: 12, name: 'canteen Coupon', Tokens: 180, buyed: 0 },
+        { id: 4, available: 8, name: 'cloth Coupon', Tokens: 109, buyed: 0 },
+        ]);
+
+    const links = [
+        { id: 1, name: "Ledger", link: "/ledger" },
+        { id: 2, name: "Transaction", link: "/transaction" },
+        { id: 3, name: "Student Dashboard", link: "/student" }
     ]
+
+    const handleBuyClick = (id) => {
+        setCouponData((prevCouponData) =>
+            prevCouponData.map((item) =>
+            item.id === id ? { ...item, buyed: 1 } : item
+            )
+        );
+        };
     return (
         <>
-        <Navbar links={links}/>
-        <div>CouponBazaar</div>
+
+            <Navbar links={links} />
+            <div className="container " >
+                <ul class="list-group w-25" style={{marginLeft:"60rem", marginTop:"2rem"}}>
+                    {couponData.map((item) => (
+                        item.buyed<1 &&
+                        <li className="list-group-item d-flex justify-content-between align-items-center">
+                        {item.name}
+                        <span className="badge bg-primary rounded-pill">{item.available}</span>
+                        <button type="button" className="btn btn-info btn-sm" onClick={()=>{handleBuyClick(item.id)}}>Buy</button>
+
+                    </li>
+                    ))}
+
+                    </ul>
+                </div>
         </>
     )
 }
